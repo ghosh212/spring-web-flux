63fa35cede08f0dfa2f5a12fc74c4c3fcf3e2204
Deborpita Ghosh <deborpitaghosh57@gmail.com>
2022-11-16 15:50:35 +0530

    Initial commit

---
+++ src/test/java/com/springwebflux/springwebflux/SpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebflux.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
